ecc625478a7584105e9ee4405bdbc18138df32f1
oyatillouzbek <63519552+oyatillouzbek@users.noreply.github.com>
2020-05-02 07:17:59 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,39 +9,7 @@ app = Client(
     api_id=api_id,
     api_hash=api_hash)
     
-RUNNING = "**Eval Expression:**\n```{}```\n**Running...**"
-ERROR = "**Eval Expression:**\n```{}```\n**Error:**\n```{}```"
-SUCCESS = "**Eval Expression:**\n```{}```\n**Success**"
-RESULT = "**Eval Expression:**\n```{}```\n**Result:**\n```{}```"
 
-@app.on_message(Filters.command("eval", "!"))
-def eval_expression(client, message):
-    expression = " ".join(message.command[1:])
-
-    if expression:
-        m = message.reply(RUNNING.format(expression))
-
-        try:
-            result = eval(expression)
-        except Exception as error:
-            client.edit_message_text(
-                m.chat.id,
-                m.message_id,
-                ERROR.format(expression, error)
-            )
-        else:
-            if result is None:
-                client.edit_message_text(
-                    m.chat.id,
-                    m.message_id,
-                    SUCCESS.format(expression)
-                )
-            else:
-                client.edit_message_text(
-                    m.chat.id,
-                    m.message_id,
-                    RESULT.format(expression, result)
-                )
 @app.on_message(Filters.new_chat_members)
 def newuser(client,message):
     chatidi = str(message.chat.id)
